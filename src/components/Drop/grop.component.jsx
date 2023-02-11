c7aab01ee8b3c68b9626d8c0dd24b7a349b4d535
teshomenbret <teshomenbret4@gamil.com>
2023-02-11 14:53:55 +0300

    all the remaining staff

---
+++ src/components/Drop/grop.component.jsx
@@ -0,0 +1,52 @@
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import three_dot from '../../asset/three_dot.svg'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Drop({choice, showModal}) {
+  return (
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="">
+                      <img
+                        className="h-5 w-4 rounded-full"
+                        src={three_dot}
+                        alt=""
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                     {   choice&&choice.map((cho,index ) =>( 
+                                          <Menu.Item key={index}>
+                                            {({ active}) => (
+                                              <a
+                                                href="#"
+                                                className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                              >
+                                                {cho}
+                                              </a>
+                                            )}
+                                          </Menu.Item>
+
+                        ))
+                    }
+
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+   
+  )
+}
--- src/pages/Field/field.pages.jsx
+++ src/pages/Field/field.pages.jsx
@@ -2,25 +2,28 @@ import React, {useState, useEffect} from 'react'
 import {farms} from '../../api/farm.api'
 import FieldInfoCard from '../../components/Cards/FieldInfoCard/fieldInfo.component';
 import SensorCard from '../../components/Cards/SensorCard/sensorCard.componet'
+import ResetGDDForm from '../../pages/Form/ResetGDD/resetGDDForm.page'
+import landscape from "../../asset/landscape.svg"
+
 
 export default function  Field(){
     const [farm,setData]=useState();
+
     useEffect(()=>{
         farms().then(farm =>setData(farm))
       },[])
     return(
-        <div>
-            <div>
-                <p>{farm&&farm[0].fields[0].altitude+" above sea level"}</p>
-            </div>
-
-            <div>
-                <FieldInfoCard/>
-            </div>
+        <div className='bg-gray-50 lg:mx-96 sm:mx-80 md:mx-10'>
+            <div className='flex justify-start mx-4 mb-4'>
+                <img src={landscape} alt="" />
+                <p className='ml-8'>{farm&&farm[0].fields[0].altitude+" above sea level"}</p>
+            </div>          
+            <FieldInfoCard/>
+            <p className='mx-8'>Last sensor reset: 04/03/22 (auto reset)</p>
            
             <div>
-                <p>Sensors</p>
-                <div>
+                <p className='mx-8 font-bold'>Sensors</p>
+                <div className='flex justify-start flex-wrap mt-3'>
                     {   farm&&farm[0].fields[0].sensors.map((sensor,index ) =>( 
                         <div key={index}>
                             <SensorCard sensor = {sensor}/>
