da78a65826a3914981151ab484a3e9b421b2c04c
unknown <petrosbeyene611@gmail.com>
2023-02-12 00:00:42 +0300

    Add line chart

--- src/components/Cards/FieldInfoCard/fieldInfo.component.jsx
+++ src/components/Cards/FieldInfoCard/fieldInfo.component.jsx
@@ -4,20 +4,37 @@ import React from 'react'
 
 export default function FieldInfoCard(props){
     return (
-        <div className=' mx-4 flex h-22 py-5 bg-white justify-evenly rounded'>
-            <div>
-                <p className='font-medium'>367</p>
-                <p>Current GDD</p>
+        
+        <div className='md:container md:mx-auto bg-white h-36 rounded-lg'>
+          <div className='flex flex-row justify-between'>
+            <div className='flex flex-col  p-10'>
+              <p className='text-4xl text-center text-gray-500'>367</p>
+              <p className='text-gray-500 '> Current GDD </p>
             </div>
-            <div>
-                <p>475</p>
-                <p>Goal GDD</p>
+            <div className='flex flex-col p-10'>
+              <p className='text-4xl text-center text-gray-500'>367</p>
+              <p className='text-gray-500 '> Current GDD </p>
             </div>
-            <div>
-                <p>{5+"days"}</p>
-                <p>To cutting </p>
-            </div>  
+            <div className='flex flex-col p-10'>
+              <p className='text-4xl text-center text-gray-500'>367</p>
+              <p className='text-gray-500 '> 5 days </p>
+            </div>
+          </div>
         </div>
+        // <div className=' mx-4 flex h-22 py-5 bg-white justify-evenly rounded'>
+        //     <div>
+        //         <p className='font-medium'>367</p>
+        //         <p>Current GDD</p>
+        //     </div>
+        //     <div>
+        //         <p>475</p>
+        //         <p>Goal GDD</p>
+        //     </div>
+        //     <div>
+        //         <p>{5+"days"}</p>
+        //         <p>To cutting </p>
+        //     </div>  
+        // </div>
         
     )
 }
--- src/pages/Field/field.pages.jsx
+++ src/pages/Field/field.pages.jsx
@@ -1,38 +1,60 @@
 import React, {useState, useEffect} from 'react'
 import {farms} from '../../api/farm.api'
+import TerrainIcon from '@mui/icons-material/Terrain'
 import FieldInfoCard from '../../components/Cards/FieldInfoCard/fieldInfo.component';
 import SensorCard from '../../components/Cards/SensorCard/sensorCard.componet'
 import ResetGDDForm from '../../pages/Form/ResetGDD/resetGDDForm.page'
 import landscape from "../../asset/landscape.svg"
-
+import NavBar from '../../components/NavBar/NavBar.component';
+import LineChart from '../../components/Cards/LineChart/CardLineChart.component';
+import ChartLineChart from '../../components/Cards/LineChart/CardLineChart.component'
 
 export default function  Field(){
-    const [farm,setData]=useState();
+    // const [farm,setData]=useState();
 
-    useEffect(()=>{
-        farms().then(farm =>setData(farm))
-      },[])
+    // useEffect(()=>{
+    //     farms().then(farm =>setData(farm))
+    //   },[])
     return(
-        <div className='bg-gray-50 lg:mx-96 sm:mx-80 md:mx-10'>
-            <div className='flex justify-start mx-4 mb-4'>
-                <img src={landscape} alt="" />
-                <p className='ml-8'>{farm&&farm[0].fields[0].altitude+" above sea level"}</p>
-            </div>          
-            <FieldInfoCard/>
-            <p className='mx-8'>Last sensor reset: 04/03/22 (auto reset)</p>
-           
-            <div>
-                <p className='mx-8 font-bold'>Sensors</p>
-                <div className='flex justify-start flex-wrap mt-3'>
-                    {   farm&&farm[0].fields[0].sensors.map((sensor,index ) =>( 
-                        <div key={index}>
-                            <SensorCard sensor = {sensor}/>
-                        </div>  
-                        ))
-                    }
+        <>
+        <div className='bg-gray-50 bg-g lg:mx-60 h-screen px-6'>
+            
+                <div className="justify-between md:items-center md:flex mb-8 ">
+                <div className="flex items-center space-x-4">
+                    <TerrainIcon />
+                    <p className="text-gray-700 text-base ">127m above see level.</p>
                 </div>
+                </div>
+            <FieldInfoCard/>
+            <div className='mt-3'>
+            <ChartLineChart />
             </div>
         </div>
+        </>
     )
     
 }
+
+
+
+
+// <div className="md:container md:mx-auto bg-slate-50 h-screen pt-6">
+//     <div className='flex justify-start mx-4 mb-4'>
+//         <img src={landscape} alt="" />
+//         <p className='ml-8'>{farm&&farm[0].fields[0].altitude+" above sea level"}</p>
+//     </div>          
+//     <FieldInfoCard/>
+//     <p className='mx-8'>Last sensor reset: 04/03/22 (auto reset)</p>
+    
+//     <div>
+//         <p className='mx-8 font-bold'>Sensors</p>
+//         <div className='flex justify-start flex-wrap mt-3'>
+//             {   farm&&farm[0].fields[0].sensors.map((sensor,index ) =>( 
+//                 <div key={index}>
+//                     <SensorCard sensor = {sensor}/>
+//                 </div>  
+//                 ))
+//             }
+//         </div>
+//     </div>
+// </div>
